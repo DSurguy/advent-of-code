700861fb6bcd98fa2e5a0681a6a30e06d1f365e9
DSurguy <Surguy.Derek@gmail.com>
2019-11-25 21:11:16 -0600

    Add solution to 2018.2

---
+++ 2018/2/index.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const { createReadStream } = fs;
+const path = require('path');
+
+let duoCount = 0;
+let triCount = 0;
+const masks = {};
+let matchedMask = undefined;
+
+function main(){
+  return new Promise((resolve, reject) => {
+    const readable = createReadStream(
+      path.resolve(__dirname, 'input')
+    )
+
+    let leftovers = '';
+    readable.on('data', chunk => {
+      const currentData = leftovers + chunk.toString();
+      const parts = currentData.split(/\n/g);
+      leftovers = parts.splice(parts.length-1, 1)[0];
+      for( let boxId of parts ){
+        incrementCountsIfMatch(boxId);
+        if( !matchedMask ) generateMasks(boxId);
+      }
+    });
+    readable.on('end', () => {
+      //finish up the leftovers
+      if( leftovers.length ){
+        incrementCountsIfMatch(leftovers);
+        if( !matchedMask ) generateMasks(boxId);
+      }
+    });
+    readable.on('close', () => {
+      console.log(`Final duo count: ${duoCount}`);
+      console.log(`Final tri count: ${triCount}`);
+      console.log(`Final checksum: ${duoCount*triCount}`);
+      resolve();
+    });
+    readable.on('error', err => {
+      reject(err);
+    })
+  })
+}
+
+function incrementCountsIfMatch(boxId){
+  const charCounts = {};
+  for( let i=0; i<boxId.length; i++ ){
+    charCounts[boxId[i]] = (charCounts[boxId[i]] || 0 ) + 1;
+  }
+  const counts = Object.values(charCounts);
+  let duo = tri = false;
+  for( let count of counts ){
+    if( count === 2 ) duo = true
+    if( count === 3 ) tri = true
+  }
+  if( duo ) duoCount++;
+  if( tri ) triCount++;
+}
+
+async function generateMasks(boxId){
+  for( let i=0; i<boxId.length; i++ ){
+    let mask = boxId.substr(0, i) + '?' + boxId.substr(i+1);
+    if( masks[mask] ){
+      //this mask was already found, return this result.
+      console.log(`Box ID 1: ${masks[mask]}`);
+      console.log(`Box ID 2: ${boxId}`);
+      matchedMask = mask.replace('?','');
+      console.log(`Mask: ${matchedMask}`);
+      return;
+    }
+    masks[mask] = boxId;
+  }
+}
+
+main();
