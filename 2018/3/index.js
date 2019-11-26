e5e0fbb482f9706fa0935f6dd87abce88cc79da9
DSurguy <Surguy.Derek@gmail.com>
2019-11-26 08:39:57 -0600

    Implement solution to 2018.3.1

---
+++ 2018/3/index.js
@@ -0,0 +1,72 @@
+const { createReadStream } = require('fs');
+const path = require('path');
+
+const grid = {}
+let overlapTotal = 0;
+
+function main(){
+  return new Promise((resolve, reject) => {
+    const readable = createReadStream(
+      path.resolve(__dirname, 'input')
+    )
+
+    let leftovers = '';
+    readable.on('data', chunk => {
+      const currentData = leftovers + chunk.toString();
+      const parts = currentData.split(/\n/g);
+      leftovers = parts.splice(parts.length-1, 1)[0];
+      for( let row of parts ){
+        crawlRectangle(
+          getRectangleDefinition(row)
+        )
+      }
+    });
+    readable.on('end', () => {
+      //finish up the leftovers
+      if( leftovers.length ){
+        crawlRectangle(
+          getRectangleDefinition(leftovers)
+        )
+      }
+    });
+    readable.on('close', () => {
+      console.log(`Final overlap total: ${overlapTotal}`);
+      resolve();
+    });
+    readable.on('error', err => {
+      reject(err);
+    })
+  })
+}
+
+function getRectangleDefinition(str){
+  const rectParts = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/.exec(str);
+  return {
+    id: rectParts[1],
+    x: parseInt(rectParts[2], 10),
+    y: parseInt(rectParts[3], 10),
+    width: parseInt(rectParts[4], 10),
+    height: parseInt(rectParts[5], 10)
+  }
+}
+
+function crawlRectangle(rect){
+  for( let x=rect.x; x<rect.x+rect.width; x++ ){
+    for( let y=rect.y; y<rect.y+rect.height; y++ ){
+      if( !grid[x] ) grid[x] = {};
+      if( !grid[x][y] ) grid[x][y] = [];
+      grid[x][y].push(rect.id);
+      if( grid[x][y].length === 2 ){
+        //this is the first time this cell has overlapped
+        overlapTotal++;
+      }
+    }
+  }
+}
+
+main()
+.then(() => console.log('complete'))
+.catch(e => {
+  console.error(e);
+  process.exit(1);
+});
