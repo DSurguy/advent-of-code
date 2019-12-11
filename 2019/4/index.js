ace7f4eb0a4f0d6ce136a645f1429abbfe79fa36
DSurguy <Surguy.Derek@gmail.com>
2019-12-10 23:21:56 -0600

    Solution to 2019.4.1

--- 2019/4/index.js
+++ 2019/4/index.js
@@ -1,8 +1,30 @@
 const argv = require('yargs-parser')(process.argv.slice(2))
-const input = require("./inputs.json");
+const inputs = require("./inputs.json");
+
+const validPasswords = [];
 
 async function main(){
-  console.log(input)
+  for( let password = inputs.min; password <= inputs.max; password++ ){
+    if( isValidPassword(password.toString()) ){
+      validPasswords.push(password)
+    }
+  }
+  console.log(`${validPasswords.length} valid passwords`);
+}
+
+function isValidPassword(password){
+  let hasDouble = false;
+  let hasDecreased = false;
+
+  for( let i=0; i<password.length; i++ ){
+    if( password[i-1] === password[i] ) hasDouble = true;
+    if( password[i-1] !== undefined && password[i-1] > password[i] ) hasDecreased = true;
+  }
+
+  if( hasDouble && !hasDecreased ){
+    return true;
+  }
+  return false;
 }
 
 main();
