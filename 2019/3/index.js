cbae4bc0e6162d7bb44a71a2aa3f358443af402d
DSurguy <Surguy.Derek@gmail.com>
2019-12-10 21:24:28 -0600

    Puzzle and cleanup for 2019.3.1

--- 2019/3/index.js
+++ 2019/3/index.js
@@ -14,50 +14,7 @@ async function main(){
     process.exit(1);
   }
 
-  if( !argv.part || argv.part === 1 ){
-    console.log(`Final value at position 0: ${processInputs(12, 2)}`)
-  }
-  else{
-    for( let first = 0; first <= 99; first++ ){
-      for( let second = 0; second <= 99; second++ ){
-        const result = processInputs(first, second);
-        if( result === 19690720 ){
-          console.log(`Inputs resulting in 19690720: [${first},${second}]`);
-          console.log(`100 * noun + verb: ${100*first+second}`);
-          process.exit(0);
-        }
-      }
-    }
-    console.log(`Processed all inputs, unable to reach solution.`)
-  }
-}
-
-function processInputs(first, second){
-  const localInts = ints.slice(0);
-  localInts[1] = first;
-  localInts[2] = second;
-
-  let opcodeIndex = 0;
-  while( opcodeIndex < localInts.length && localInts[opcodeIndex] && localInts[opcodeIndex] !== 99 ){
-    const inputs = [
-      localInts[localInts[opcodeIndex+1]],
-      localInts[localInts[opcodeIndex+2]]
-    ]
-    const destination = localInts[opcodeIndex+3];
-    switch(localInts[opcodeIndex]){
-      case 1:
-        //addition
-        localInts[destination] = inputs[0] + inputs[1];
-      break;
-      case 2:
-        //multiplication
-        localInts[destination] = inputs[0] * inputs[1];
-      break;
-    }
-    opcodeIndex += 4;
-  }
-
-  return localInts[0]
+  
 }
 
 main();
