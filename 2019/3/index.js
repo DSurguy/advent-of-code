0417632d86af77977f4d831c37bf59dd471806e9
DSurguy <Surguy.Derek@gmail.com>
2019-12-10 22:26:43 -0600

    Solution to 2019.3.1

--- 2019/3/index.js
+++ 2019/3/index.js
@@ -1,13 +1,37 @@
-const { createReadStream } = require('fs');
-const path = require('path');
 const argv = require('yargs-parser')(process.argv.slice(2))
 const gatherInput = require('../../utils/gatherInput.js');
 
-let ints = [];
+const visitedMap = {};
+let currentShortestDistance;
+let currentPosition = {
+  x: 0,
+  y: 0
+};
+let currentShortestPoint = {
+  x: 0,
+  y: 0
+}
+let currentWire = 0;
 
 async function main(){
   try{
-    await gatherInput((val) => { ints.push(parseInt(val, 10)) }, /,/g)
+    await gatherInput(
+      `2019/3/${argv.input || 'input'}`,
+      (instruction, isNewWire) => { 
+        if( isNewWire ){
+          currentWire++;
+          currentPosition = {
+            x: 0,
+            y: 0
+          }
+        }
+        layWire(instruction)
+      },
+      /,/g
+    )
+
+    console.log(`Shortest Manhattan Distance: ${currentShortestDistance}`)
+    console.log(`Position of closest cross: [${currentShortestPoint.x},${currentShortestPoint.y}]`)
   }
   catch (e){
     console.error(e);
@@ -15,6 +39,56 @@ async function main(){
   }
 
   
+}
+
+function layWire(instruction){
+  const direction = instruction[0];
+  const amount = parseInt(instruction.substr(1), 10);
+  switch(direction){
+    case "R":
+      for( let i=0; i<amount; i++ ){
+        currentPosition.x += 1;
+        checkCurrentPosition();
+      }
+      break;
+    case "L":
+      for( let i=0; i<amount; i++ ){
+        currentPosition.x -= 1;
+        checkCurrentPosition();
+      }
+      break;
+    case "D":
+      for( let i=0; i<amount; i++ ){
+        currentPosition.y -= 1;
+        checkCurrentPosition();
+      }
+      break;
+    case "U":
+      for( let i=0; i<amount; i++ ){
+        currentPosition.y += 1;
+        checkCurrentPosition();
+      }
+  }
+}
+
+function checkCurrentPosition(){
+  if( !visitedMap[currentPosition.x] ) visitedMap[currentPosition.x] = {};
+  if( currentWire === 2 && visitedMap[currentPosition.x][currentPosition.y] ){
+    //check to see if this is the new closest point
+    const currentDistance = Math.abs(currentPosition.x) + Math.abs(currentPosition.y);
+    if( 
+      currentShortestDistance === undefined
+      || currentDistance < currentShortestDistance
+    ){
+      //update our winner
+      currentShortestDistance = currentDistance;
+      Object.assign(currentShortestPoint, currentPosition);
+    }
+  }
+  else if( currentWire == 1){
+    //this is the first time we have visited this node
+    visitedMap[currentPosition.x][currentPosition.y] = true
+  }
 }
 
 main();
--- utils/gatherInput.js
+++ utils/gatherInput.js
@@ -1,16 +1,25 @@
-module.exports = async function (partHandler, splitOn){
+const { createReadStream } = require('fs');
+const path = require('path');
+
+module.exports = async function (inputPath, partHandler, splitOn){
   return new Promise((resolve, reject) => {
     const readable = createReadStream(
-      path.resolve(__dirname, 'input')
+      path.resolve(__dirname, '../', inputPath)
     )
 
     let leftovers = '';
+    let isLine = true;
     readable.on('data', chunk => {
       const currentData = leftovers + chunk.toString();
-      const parts = currentData.split(splitOn);
-      leftovers = parts.splice(parts.length-1, 1)[0];
-      for( let val of parts ){
-        partHandler(val);
+      const lines = currentData.split(/[\n\r]+/g).filter(line => line.length > 0);
+      for( let lineIndex=0; lineIndex<lines.length; lineIndex++ ){
+        if(lines[lineIndex-1] !== undefined ) isLine = true; //this is at least the second line here
+        const parts = lines[lineIndex].split(splitOn);
+        if( lines[lineIndex+1] === undefined ) leftovers = parts.splice(parts.length-1, 1)[0]; //last line
+        for( let val of parts ){
+          partHandler(val, isLine);
+          isLine = false;
+        }
       }
     });
     readable.on('end', () => {
