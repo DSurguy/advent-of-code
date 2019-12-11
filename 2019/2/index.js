bf18977cb4151c58b8f823157e76df3f8537060c
DSurguy <Surguy.Derek@gmail.com>
2019-12-10 20:54:31 -0600

    Solution to 2019.2.2

--- 2019/2/index.js
+++ 2019/2/index.js
@@ -1,5 +1,6 @@
 const { createReadStream } = require('fs');
 const path = require('path');
+const argv = require('yargs-parser')(process.argv.slice(2))
 
 let ints = [];
 
@@ -33,32 +34,52 @@ function main(){
   })
 }
 
-main()
-.then(() => {
-  //Pre-conditioning the code 1202
-  ints[1] = 12;
-  ints[2] = 2;
-  //Off we go
+function processInputs(first, second){
+  const localInts = ints.slice(0);
+  localInts[1] = first;
+  localInts[2] = second;
+
   let opcodeIndex = 0;
-  while( opcodeIndex < ints.length && ints[opcodeIndex] && ints[opcodeIndex] !== 99 ){
+  while( opcodeIndex < localInts.length && localInts[opcodeIndex] && localInts[opcodeIndex] !== 99 ){
     const inputs = [
-      ints[ints[opcodeIndex+1]],
-      ints[ints[opcodeIndex+2]]
+      localInts[localInts[opcodeIndex+1]],
+      localInts[localInts[opcodeIndex+2]]
     ]
-    const destination = ints[opcodeIndex+3];
-    switch(ints[opcodeIndex]){
+    const destination = localInts[opcodeIndex+3];
+    switch(localInts[opcodeIndex]){
       case 1:
         //addition
-        ints[destination] = inputs[0] + inputs[1];
+        localInts[destination] = inputs[0] + inputs[1];
       break;
       case 2:
         //multiplication
-        ints[destination] = inputs[0] * inputs[1];
+        localInts[destination] = inputs[0] * inputs[1];
       break;
     }
     opcodeIndex += 4;
   }
-  console.log(`Final value at position 0: ${ints[0]}`)
+
+  return localInts[0]
+}
+
+main()
+.then(() => {
+  if( !argv.part || argv.part === 1 ){
+    console.log(`Final value at position 0: ${processInputs(12, 2)}`)
+  }
+  else{
+    for( let first = 0; first <= 99; first++ ){
+      for( let second = 0; second <= 99; second++ ){
+        const result = processInputs(first, second);
+        if( result === 19690720 ){
+          console.log(`Inputs resulting in 19690720: [${first},${second}]`);
+          console.log(`100 * noun + verb: ${100*first+second}`);
+          process.exit(0);
+        }
+      }
+    }
+    console.log(`Processed all inputs, unable to reach solution.`)
+  }
 })
 .catch(e => {
   console.error(e);
