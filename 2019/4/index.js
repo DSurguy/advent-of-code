76f7dfac60d5769c8c7430ae208b6a89d4ef909a
DSurguy <Surguy.Derek@gmail.com>
2019-12-10 23:41:03 -0600

    Solution for 2019.4.2

--- 2019/4/index.js
+++ 2019/4/index.js
@@ -16,15 +16,42 @@ function isValidPassword(password){
   let hasDouble = false;
   let hasDecreased = false;
 
+  let currentChainCount = 0;
+  let currentChainValue;
   for( let i=0; i<password.length; i++ ){
-    if( password[i-1] === password[i] ) hasDouble = true;
-    if( password[i-1] !== undefined && password[i-1] > password[i] ) hasDecreased = true;
+    const currentValue = password[i];
+
+    //Check to see if we've descreased in value
+    if( currentChainValue !== undefined && currentChainValue > currentValue ) hasDecreased = true;
+
+    //Check to see if we're chaining the same number
+    if( currentValue === currentChainValue ){
+      //the characters were the same, bump the chain count
+      currentChainCount++;
+      //If we're at the end, handle it special
+      if( i === password.length-1 && currentChainCount === 2) hasDouble = true;
+    }
+    else{
+      //the current and last number were different
+      if( chainCountsAsDouble(currentChainCount) ) hasDouble = true; //We had a chain of 2 before this, so there's a real double
+      currentChainCount = 1; //reset the chain count
+      currentChainValue = currentValue; //change the chain character
+    }
   }
 
   if( hasDouble && !hasDecreased ){
     return true;
   }
   return false;
+}
+
+function chainCountsAsDouble(chainCount){
+  if ( argv.part === 2 ){
+    return chainCount === 2;
+  }
+  else {
+    return chainCount >= 2;
+  }
 }
 
 main();
