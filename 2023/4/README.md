2f1032835def4f3a7075bb2a990dac3e04e28689
Derek Surguy <Surguy.Derek@gmail.com>
2024-03-06 01:07:36 -0600

    Solve day 4 part 2

--- 2023/4/README.md
+++ 2023/4/README.md
@@ -1,5 +1,7 @@
 # Day 4 Mock Answers
 - Part One: 13
+- Part Two: 30
 
 # Day 4 Actual Answers
 - Part One: 15268
+- Part Two: 6283755
--- 2023/4/day4.ts
+++ 2023/4/day4.ts
@@ -10,7 +10,7 @@ const lines = (await inputFile.text()).split(/\n/g);
 const parseLine = (line: string) => {
   // Line Structure: "Card  11: 12 34 56 78 90 | 83 86  6 31 17  9 48 53"
   const parts = line.split(/Card\s+|:\s|\s+\|\s+/g).slice(1) as [string, string, string];
-  const card = parts[0];
+  const card = Number(parts[0]);
   const winningNumbers = new Set(parts[1].split(/\s+/g).map(Number));
   // This may not have to be a set for part two. Who knows!
   const numbers = new Set(parts[2].split(/\s+/g).map(Number));
@@ -25,9 +25,27 @@ const parseLine = (line: string) => {
   }
 }
 
+let totalPoints = 0;
+const cardCount = new Map<number, number>();
+lines.forEach(line => {
+  const parsedLine = parseLine(line);
+  totalPoints += parsedLine.value;
+  const finalThisCardCount = (cardCount.get(parsedLine.card) || 0) + 1;
+  cardCount.set(parsedLine.card, finalThisCardCount);
+  for( let i = 0; i < parsedLine.matchedNumbers.length; i++ ) {
+    cardCount.set(
+      parsedLine.card + i + 1,
+      (cardCount.get(parsedLine.card + i + 1) || 0) + finalThisCardCount
+    )
+  }
+})
+
 console.log(
   "Part One",
-  lines.reduce((acc, line) => {
-    return acc + parseLine(line).value;
-  }, 0)
+  totalPoints
+)
+
+console.log(
+  "Part Two",
+  Array.from(cardCount.values()).reduce((acc, val) => acc + val, 0)
 )
